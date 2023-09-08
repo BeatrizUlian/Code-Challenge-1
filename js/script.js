3de4cf6a131cba0d209262f61dbdf291c0a86ba8
BeatrizUlian <beatrizh.ulian@gmail.com>
2023-09-07 22:04:44 -0400

    created

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function ageCheckOntario() {
+    var age = document.getElementById("age").value;
+    if (age < 16) {
+    document.getElementById("result").innerHTML = "You can not drive, visit a Casino and buy alcohol in Ontario!";
+    } else if (age >= 16 && age <=18) {
+    document.getElementById("result").innerHTML = "You can drive, but can not visit a Casino and buy alcohol in Ontario!";
+    } else if (age >= 19) {
+    document.getElementById("result").innerHTML = "You can drive, visit a Casino and buy alcohol in Ontario!";
+    }    
+}
+
+var btn = document.getElementById("submitBtn");
+btn.onclick = ageCheckOntario;
